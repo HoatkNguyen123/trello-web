26b82748321a8d04574051c55516c2aba48d9bff
HoaND97 <duyhoatk97@gmail.com>
2022-05-26 16:34:29 +0700

    design trello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className='hello-world'>Hello World</h1>
-    </div>
-  );
+	return (
+		<div className="trello-master">
+			<nav className='navbar app'>App Bar</nav>
+			<nav className='navbar board'>Board Bar</nav>
+			<div className='board-columns'>
+				<div className='column'>
+					<header>header</header>
+					<ul>
+						<li>
+							<img src='./logo192.png' alt=''></img>
+							title: trello
+						</li>
+						<li>
+							what your's name
+						</li>
+						<li>
+							what your's name
+						</li>
+						<li>
+							what your's name
+						</li>
+						<li>
+							what your's name
+						</li>
+						<li>
+							what your's name
+						</li>
+						<li>
+							what your's name
+						</li>
+					</ul>
+					<footer>footer</footer>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
